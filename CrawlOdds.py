ef46472c2279eee92f32faf9722743e25f900d1f
weihao0521 <weihao123a@gmail.com>
2015-03-19 20:12:21 -0400

    add opener

--- CrawlOdds.py
+++ CrawlOdds.py
@@ -1,3 +1,13 @@
-class CrawlOdds():
+import urllib2
+
+
+class CrawlOdds(object):
     def __init__(self):
-        pass
+        # open html page with url
+        self.opener = urllib2.build_opener()
+        self.opener.addheaders = [
+            ('User-agent', ('Mozilla/4.0 (compatible; MSIE 6.0; '
+                            'Windows NT 5.2; .NET CLR 1.1.4322)'))
+        ]
+
+
